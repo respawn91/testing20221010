4ab12e12607b3c3a7180c06639810dde182889a6
Anton Tarasov <respawn91@gmail.com>
2022-10-14 20:53:07 +0300

    Add files via upload

---
+++ Lesson-03.java
@@ -0,0 +1,166 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 100;
+      
+      System.out.println(a);
+      
+  }
+}
+
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 100;
+      System.out.println(a);
+      
+      String b = "I love Saratov";
+      System.out.println(b);
+      
+      boolean c = true;
+      System.out.println(c);
+      
+      double d = -5.3;
+      System.out.println(d);
+  }
+}
+
+
+
+<?php
+	
+	$a = 10;
+	echo $a, "\n";
+	
+	$a = -5.32;
+	echo $a, "\n";
+	
+	$a = "Hello!!!";
+	echo $a, "\n";
+	
+	$a = true;
+	echo $a, "\n";
+	echo var_export($a);
+	
+?>
+
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 50;
+      int b = 15;
+      
+      int c1 = a + b;
+      System.out.println(c1);   // 65
+      
+      int c2 = a - b;
+      System.out.println(c2);   // 35
+      
+      int c3 = a * b;
+      System.out.println(c3);   // 750
+      
+      int c4 = a / b;
+      System.out.println(c4);   // 3
+      
+      int c5 = a % b;
+      System.out.println(c5);   // 5
+      
+  }
+}
+
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      double a = 50;
+      double b = 15;
+      
+      double c1 = a + b;
+      System.out.println(c1);   // 65
+      
+      double c2 = a - b;
+      System.out.println(c2);   // 35
+      
+      double c3 = a * b;
+      System.out.println(c3);   // 750
+      
+      double c4 = a / b;
+      System.out.println(c4);   // 3.(3)
+      
+      double c5 = a % b;
+      System.out.println(c5);   // 5
+  }
+}
+
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      String s1 = "My name is ";
+      String s2 = "Anton";
+      
+      String s3 = s1 + s2;
+      System.out.println(s3);
+  }
+}
+
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      String s1 = "My name is ";
+      String s2 = "Anton";
+      String s3 = "My age is ";
+      
+      int age = 31;
+      
+      String result1 = s1 + s2;
+      System.out.println(result1);
+      
+      String result2 = s3 + age;
+      System.out.println(result2);
+      
+      String s4 = "Text ";
+      int a = 10;
+      int b = 20;
+      
+      System.out.println(s4 + a + b);
+      System.out.println(s4 + (a + b));
+  }
+}
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 10;
+      System.out.println(a);    // 10
+      
+      a++;
+      System.out.println(a);    // 11
+      
+      a = a + 1;
+      System.out.println(a);    // 12
+  }
+}
+
